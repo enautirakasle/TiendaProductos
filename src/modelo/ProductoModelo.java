2b7307f814ce4d0b1fe5e5833c7442976b35a8a0
eñaut <enaut.irakasle@gmail.com>
2018-06-15 11:45:31 +0200

    update implementado

--- src/modelo/ProductoModelo.java
+++ src/modelo/ProductoModelo.java
@@ -59,7 +59,17 @@ public class ProductoModelo extends Conector{
 	}
 
 	public void update(Producto producto) {
-		// TODO Auto-generated method stub
+		try {
+			PreparedStatement pst = super.conexion.prepareStatement("update productos set nombre=?, fecha_compra=?, precio=? where id=?");
+			pst.setString(1, producto.getNombre());
+			pst.setDate(2, new java.sql.Date(producto.getFechaCompra().getTime()));
+			pst.setDouble(3, producto.getPrecio());
+			pst.setInt(4, producto.getId());
+			
+			pst.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
